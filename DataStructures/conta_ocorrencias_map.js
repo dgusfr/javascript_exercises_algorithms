bb0309b36bdae8dd86b2f8bc9e7f88c88ea9f008
dgusfr <diegogustavo.fr@gmail.com>
2024-10-17 19:01:59 -0300

    feat(MAP0: frurta

---
+++ DataStructures/conta_ocorrencias_map.js
@@ -0,0 +1,27 @@
+// # Dado um array de palavras, crie um objeto Map que conte quantas vezes cada palavra aparece no array. Exiba o resultado.
+// # javascript
+// # Copiar código
+// # const palavras = ['maçã', 'banana', 'maçã', 'laranja', 'banana', 'banana', 'uva'];
+
+// # A saída esperada seria algo como:
+
+// # maçã: 2
+// # banana: 3
+// # laranja: 1
+// # uva: 1
+
+const palavras = [
+  "maçã",
+  "banana",
+  "maçã",
+  "laranja",
+  "banana",
+  "banana",
+  "uva",
+];
+
+const fruta = new Map();
+
+for (let i = 0; i <= length(palavras); i++) {
+  fruta.set;
+}
---
+++ DataStructures/idade_futura.js
@@ -0,0 +1,4 @@
+const idades = [25, 30, 35, 40, 45];
+
+let idade_futuras = idades.map((idade) => idade + 5);
+console.log(idade_futuras);
