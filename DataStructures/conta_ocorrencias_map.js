6f2041e20f8d2f16733fd7a5ac2785e2f0be38e9
dgusfr <diegogustavo.fr@gmail.com>
2024-10-17 19:12:18 -0300

    feat(map): contagem de fruta

--- DataStructures/conta_ocorrencias_map.js
+++ DataStructures/conta_ocorrencias_map.js
@@ -1,27 +1,16 @@
-// # Dado um array de palavras, crie um objeto Map que conte quantas vezes cada palavra aparece no array. Exiba o resultado.
-// # javascript
-// # Copiar código
-// # const palavras = ['maçã', 'banana', 'maçã', 'laranja', 'banana', 'banana', 'uva'];
+const frutas = ["maçã", "banana", "maçã", "laranja", "banana", "banana", "uva"];
 
-// # A saída esperada seria algo como:
+const frutaContagem = new Map();
 
-// # maçã: 2
-// # banana: 3
-// # laranja: 1
-// # uva: 1
+frutas.forEach((fruta) => {
+  if (frutaContagem.has(fruta)) {
+    const contador = frutaContagem.get(fruta);
+    frutaContagem.set(fruta, contador + 1);
+  } else {
+    frutaContagem.set(fruta, 1);
+  }
+});
 
-const palavras = [
-  "maçã",
-  "banana",
-  "maçã",
-  "laranja",
-  "banana",
-  "banana",
-  "uva",
-];
-
-const fruta = new Map();
-
-for (let i = 0; i <= length(palavras); i++) {
-  fruta.set;
-}
+frutaContagem.forEach((contador, fruta) => {
+  console.log(`${fruta} : ${contador}`);
+});
