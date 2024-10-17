9ec8dfb9fa7bf301cfccbf66b9f95f59e8e4cfd4
dgusfr <diegogustavo.fr@gmail.com>
2024-10-17 13:39:48 -0300

    feat(arrays)

---
+++ DataStructures/array.js
@@ -0,0 +1,15 @@
+const numerosInteiros = [5, 12, 7, 34, 2, 19, 23, 8, 10, 3];
+soma_dos_numero(numerosInteiros);
+
+function soma_dos_numero(numerosInteiros) {
+  numerosInteiros.array.forEach((numero) => {
+    soma += numero;
+  });
+  console.log(soma);
+}
+
+function maior_menor() {}
+
+function remove_duplicata() {}
+
+function inverter_array() {}
