533363a8e64b7ab1b09ce7cc5b022f394619d05b
dgusfr <diegogustavo.fr@gmail.com>
2024-10-17 16:51:05 -0300

    feat(obj): manipulacao_objetos

---
+++ dataTypes/manipulacao_objetos.js
@@ -0,0 +1,15 @@
+const carro = {
+  marca: "Toyota",
+  modelo: "Corolla",
+  ano: 2022,
+};
+
+carro.cor = "vermelho";
+
+carro.ano = 2024;
+
+if (carro.hasOwnProperty("marca") === true) {
+  console.log("O carro tem a propriedade marca");
+} else {
+  console.log("O carro NÃO tem a propriedade marca");
+}
