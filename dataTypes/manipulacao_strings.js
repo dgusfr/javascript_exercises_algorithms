4d1325d059705a9e913ee7fee8ec2ab5d7c7059d
dgusfr <diegogustavo.fr@gmail.com>
2024-10-17 16:32:18 -0300

    feat(string): manipulation

---
+++ dataTypes/manipulacao_strings.js
@@ -0,0 +1,10 @@
+let frase = "Ocorreu um erro ao processar a solicitação.";
+
+cinco_primeiros = frase.slice(0, 5);
+console.log(cinco_primeiros);
+
+frase_corrigida = frase.replace("erro", "correção");
+console.log(frase_corrigida);
+
+frase_dividida = frase.split(" ");
+console.log(frase_dividida);
