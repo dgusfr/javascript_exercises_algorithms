a28a13196d5e4e9f2861b8477b74c73dc03a6b58
dgusfr <diegogustavo.fr@gmail.com>
2024-10-18 08:25:51 -0300

    feat(ordem)

---
+++ Functions/ordem_superior.js
@@ -0,0 +1,7 @@
+function aplicarOperacaoEmArray(array, operacao) {
+  // Sua implementação aqui
+}
+function dobrar(x) {
+  return x * 2;
+}
+console.log(aplicarOperacaoEmArray([1, 2, 3, 4], dobrar)); // Saída esperada: [2, 4, 6, 8]
